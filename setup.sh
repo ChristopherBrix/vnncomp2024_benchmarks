cc766b2e4c99c4be83422ae3dddb8106d1ad98ec
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2024-07-07 19:53:15 +0200

    Support large models for 2024

--- setup.sh
+++ setup.sh
@@ -1,2 +1,18 @@
+wget https://rwth-aachen.sciebo.de/s/Erh54iS6fT3hHCr/download -O large_models.zip
+unzip large_models.zip -d large_models
+
+echo "Moving large benchmark files"
+cd large_models/vnncomp2024/
+for d in *
+do
+    cd $d/seed_896832480/;
+    mkdir -p ../../../../benchmarks/$d/onnx
+    mkdir -p ../../../../benchmarks/$d/vnnlib
+    find . -type f -exec mv "{}" "../../../../benchmarks/$d/{}" \;
+    cd ../../;
+done
+cd ../..
+rm -r large_models large_models.zip
+
 echo "Unzipping"
 gunzip benchmarks/*/onnx/* benchmarks/*/onnx/*/* benchmarks/*/vnnlib/* benchmarks/*/vnnlib/*/*
